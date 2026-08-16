f8eff897a201ed6cd9e6bddd60ff15e022603087
mca2026aayush-coder <mca2026.aayush@unigoa.ac.in>
2026-08-16 18:09:01 +0530

    Delete Java/Java directory

--- Java/Java/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\Users\Aayush Mahale\.antigravity\extensions\redhat.java-1.55.0-win32-x64\jre\21.0.11-win32-x86_64\bin\javac.exe" *.java
-if %errorlevel% equ 0 (
-    "C:\Users\Aayush Mahale\.antigravity\extensions\redhat.java-1.55.0-win32-x64\jre\21.0.11-win32-x86_64\bin\java.exe" Main
-)
